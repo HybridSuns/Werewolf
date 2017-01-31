c87aee77c337830a9e22792e33f2eb94470d5b93
HybridSuns <litiansq89@gmail.com>
2017-01-30 23:33:25 -0600

    update

--- src/werewolf/PerformGame.java
+++ src/werewolf/PerformGame.java
@@ -14,38 +14,146 @@ public class PerformGame extends JFrame {
 			players[i] = new Player(i, true, "Player");
 			System.out.printf("第%d 玩家加入\n", i);
 		}
-	
+
+		// 分配职业面板
+		JPanel pJob = new JPanel(new FlowLayout(FlowLayout.LEFT, 2, 2));
+		// 分配丘比特
+		pJob.add(new JLabel("丘比特号码："));
+		JTextField jtfCupidPlayer = new JTextField(8);
+		pJob.add(jtfCupidPlayer);
+		JButton jbtAddCupid = new JButton("分配丘比特");
+		pJob.add(jbtAddCupid);
+		jbtAddCupid.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String[] str = jtfCupidPlayer.getText().split(",");
+				for (String s : str) {
+					players[Integer.parseInt(s)].setJob("Cupid");
+					System.out.println(s + "号玩家是丘比特");
+				}
+			}
+		});
+
+		// 分配狼人
+		pJob.add(new JLabel("狼人号码："));
+		JTextField jtfWolfPlayer = new JTextField(8);
+		pJob.add(jtfWolfPlayer);
+		JButton jbtAddWolf = new JButton("分配狼人");
+		pJob.add(jbtAddWolf);
+		jbtAddWolf.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String[] str = jtfWolfPlayer.getText().split(",");
+				for (String s : str) {
+					players[Integer.parseInt(s)].setJob("Wolf");
+					System.out.println(s + "号玩家是狼人");
+				}
+			}
+		});
+
+		// 分配女巫
+		pJob.add(new JLabel("女巫号码："));
+		JTextField jtfWitchPlayer = new JTextField(8);
+		pJob.add(jtfWitchPlayer);
+		JButton jbtAddWitch = new JButton("分配女巫");
+		pJob.add(jbtAddWitch);
+		jbtAddWitch.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String[] str = jtfWitchPlayer.getText().split(",");
+				for (String s : str) {
+					players[Integer.parseInt(s)].setJob("Witch");
+					System.out.println(s + "号玩家是女巫");
+				}
+			}
+		});
+
+		// 分配预言家
+		pJob.add(new JLabel("预言家号码："));
+		JTextField jtfSeerPlayer = new JTextField(8);
+		pJob.add(jtfSeerPlayer);
+		JButton jbtAddSeer = new JButton("分配预言家");
+		pJob.add(jbtAddSeer);
+		jbtAddSeer.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String[] str = jtfSeerPlayer.getText().split(",");
+				for (String s : str) {
+					players[Integer.parseInt(s)].setJob("Seer");
+					System.out.println(s + "号玩家是预言家");
+				}
+			}
+		});
+
+		// 分配猎人
+		pJob.add(new JLabel("猎人号码："));
+		JTextField jtfHunterPlayer = new JTextField(8);
+		pJob.add(jtfHunterPlayer);
+		JButton jbtAddHunter = new JButton("分配猎人");
+		pJob.add(jbtAddHunter);
+		jbtAddHunter.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String[] str = jtfHunterPlayer.getText().split(",");
+				for (String s : str) {
+					players[Integer.parseInt(s)].setJob("Hunter");
+					System.out.println(s + "号玩家是猎人");
+				}
+			}
+		});
+		pJob.setBorder(new TitledBorder("分配职业面板"));
+
+		// 丘比特面板
+		JPanel pCupid = new JPanel(new FlowLayout(FlowLayout.LEFT, 2, 2));
+		pCupid.add(new JLabel("丘比特连的人"));
+		JTextField jtfLover1 = new JTextField(2);
+		JTextField jtfLover2 = new JTextField(2);
+		pCupid.add(jtfLover1);
+		pCupid.add(jtfLover2);
+
+		JButton jbtConnect = new JButton("确定");
+		pCupid.add(jbtConnect);
+
+		jbtConnect.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				int lover1 = Integer.parseInt(jtfLover1.getText());
+				int lover2 = Integer.parseInt(jtfLover2.getText());
+
+				players[lover1].setLover(players[lover2]);
+				players[lover2].setLover(players[lover1]);
+
+			}
+		});
+		pCupid.setBorder(new TitledBorder("丘比特面板"));
+
 		// 狼人面板
 		JPanel pWolfs = new JPanel(new FlowLayout(FlowLayout.LEFT, 2, 2));
 		pWolfs.add(new JLabel("狼人杀的玩家："));
 		JTextField jtfToKill = new JTextField(2);
 		pWolfs.add(jtfToKill);
-		
+
 		JButton jbtKill = new JButton("确定");
 		pWolfs.add(jbtKill);
-		
+
 		jbtKill.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				WolfKill(jtfToKill,players);
-				System.out.println(Integer.parseInt(jtfToKill.getText())+"玩家的状态是"+players[Integer.parseInt(jtfToKill.getText())].isAlive());
+				WolfKill(jtfToKill, players);
+				System.out.println(Integer.parseInt(jtfToKill.getText()) + "玩家的状态是"
+						+ players[Integer.parseInt(jtfToKill.getText())].isAlive());
 			}
 		});
 		pWolfs.setBorder(new TitledBorder("狼人面板"));
-		
+
 		// 女巫面板
 		JPanel pWitch = new JPanel(new FlowLayout(FlowLayout.LEFT, 2, 2));
 		pWitch.add(new JLabel("女巫救人"));
 		JCheckBox jcbSave = new JCheckBox("救人");
 		pWitch.add(jcbSave);
-		jcbSave.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent e){
-				if(jcbSave.isSelected()){
-					WitchSave(jtfToKill,players);
-					System.out.println(Integer.parseInt(jtfToKill.getText())+"玩家的状态是"+players[Integer.parseInt(jtfToKill.getText())].isAlive());	
-				}
-				else{
-					WolfKill(jtfToKill,players);
-					System.out.println(Integer.parseInt(jtfToKill.getText())+"玩家的状态是"+players[Integer.parseInt(jtfToKill.getText())].isAlive());	
+		jcbSave.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (jcbSave.isSelected()) {
+					WitchSave(jtfToKill, players);
+					System.out.println(Integer.parseInt(jtfToKill.getText()) + "玩家的状态是"
+							+ players[Integer.parseInt(jtfToKill.getText())].isAlive());
+				} else {
+					WolfKill(jtfToKill, players);
+					System.out.println(Integer.parseInt(jtfToKill.getText()) + "玩家的状态是"
+							+ players[Integer.parseInt(jtfToKill.getText())].isAlive());
 				}
 			}
 		});
@@ -53,7 +161,7 @@ public class PerformGame extends JFrame {
 		pWitch.add(jcbPoison);
 		JTextField jtfToPoison = new JTextField(2);
 		pWitch.add(jtfToPoison);
-		
+
 		jtfToPoison.setEditable(false);
 		jtfToPoison.setBackground(Color.gray);
 		JButton jbtPoison = new JButton("毒死他！");
@@ -72,10 +180,11 @@ public class PerformGame extends JFrame {
 				}
 			}
 		});
-		jbtPoison.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent e){
-				WitchPoison(jtfToPoison,players);
-				System.out.println(Integer.parseInt(jtfToPoison.getText())+"玩家的状态是"+players[Integer.parseInt(jtfToPoison.getText())].isAlive());
+		jbtPoison.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				WitchPoison(jtfToPoison, players);
+				System.out.println(Integer.parseInt(jtfToPoison.getText()) + "玩家的状态是"
+						+ players[Integer.parseInt(jtfToPoison.getText())].isAlive());
 			}
 		});
 		pWitch.setBorder(new TitledBorder("女巫面板"));
@@ -89,42 +198,44 @@ public class PerformGame extends JFrame {
 		pSeer.add(jbtSee);
 		JLabel seeRes = new JLabel("");
 		pSeer.add(seeRes);
-		jbtSee.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent e){
+		jbtSee.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
 				int numToSee = Integer.parseInt(jtfNumToSee.getText());
 				String seeJob = players[numToSee].getJob();
-				System.out.println(numToSee+"号玩家的职业是："+seeJob);
-				if(seeJob.equals("Wolf")){
+				System.out.println(numToSee + "号玩家的职业是：" + seeJob);
+				if (seeJob.equals("Wolf")) {
 					seeRes.setText("他是狼人");
-				} else{
+				} else {
 					seeRes.setText("他是好人");
 				}
 			}
 		});
-		setLayout(new GridLayout(3,1,5,5));
+		setLayout(new GridLayout(3, 1, 5, 5));
+		add(pJob);
+		add(pCupid);
 		add(pWolfs);
 		add(pWitch);
 		add(pSeer);
 	}
 
 	private void WolfKill(JTextField jtfToKill, Player[] players) {
-		int numToKill=Integer.parseInt(jtfToKill.getText());
+		int numToKill = Integer.parseInt(jtfToKill.getText());
 		players[numToKill].setAlive(false);
-		if(players[numToKill].isInLove())
+		if (players[numToKill].isInLove())
 			players[numToKill].getLover().setAlive(false);
 	}
-	
-	private void WitchPoison(JTextField jtfToPoison, Player[] players){
+
+	private void WitchPoison(JTextField jtfToPoison, Player[] players) {
 		int numToPosion = Integer.parseInt(jtfToPoison.getText());
 		players[numToPosion].setAlive(false);
-		if(players[numToPosion].isInLove())
+		if (players[numToPosion].isInLove())
 			players[numToPosion].getLover().setAlive(false);
 	}
-	
-	private void WitchSave(JTextField jtfToKill, Player[] players){
+
+	private void WitchSave(JTextField jtfToKill, Player[] players) {
 		int numToSave = Integer.parseInt(jtfToKill.getText());
 		players[numToSave].setAlive(true);
-		if(players[numToSave].isInLove())
+		if (players[numToSave].isInLove())
 			players[numToSave].getLover().setAlive(true);
 	}
 }
